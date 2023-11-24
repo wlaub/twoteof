c424e527691ca0c2781219b4e8bef31a948c4c89
wlaub <goldcray@gmail.com>
2023-11-24 14:00:07 -0700

    add critical geometry for all final cave rooms + duration estimate

--- scripts/erosion_inputs/extract_good_runs.py
+++ scripts/erosion_inputs/extract_good_runs.py
@@ -7,17 +7,19 @@ from collections import defaultdict
 import tuw
 from tuw import render
 
-infile = sys.argv[1]
-start_time = time.time()
-states = tuw.StateDump(infile)
-end_time = time.time()
+runs = []
+#infile = sys.argv[1]
+for infile in {'pristine_1.dump', 'pristine_2.dump'}:
+    start_time = time.time()
+    states = tuw.StateDump(infile)
+    end_time = time.time()
 
-print(f'{len(states.states)} states loaded in {end_time-start_time:.2f} s')
-print(states.map)
-print(states.chapter)
-print(states.rooms)
+    print(f'{len(states.states)} states loaded in {end_time-start_time:.2f} s')
+    print(states.map)
+    print(states.chapter)
+    print(states.rooms)
 
-runs = states.extract_sequences(tuw.RoomCompleteRun)
+    runs.extend(states.extract_sequences(tuw.RoomCompleteRun))
 
 print(f'{len(runs)} total runs')
 
@@ -44,6 +46,13 @@ room_offsets = {
 'g-04': (-846,456),
 'g-05': (-980,419),
 'g-06': (-1095,379),
+'g-07': (-1141,441),
+'g-08': (-1321,522),
+#'g-09': (-1455,527),
+'g-10': (-1589,502),
+'g-11': (-1746,460),
+'g-12': (-1838,440),
+'g-13': (-1960,547),
 }
 
 room_offsets = {k:(v[0]*8, v[1]*8) for k,v in room_offsets.items()}
@@ -53,6 +62,13 @@ lock_blocks = {
 'g-01': [(608,24), (120,160)],
 'g-03b': [(384,416)],
 'g-05': [(88,0)],
+'g-06': [(56,520),(80,656)],
+'g-07': [(144,48),(0,1000)],
+'g-08': [(1368,176),(888,304),(304,144)],
+'g-10': [(320,256),(432,256)],
+'g-11': [(184,320)],
+'g-12': [(368,320)],
+'g-13': [(152,176)],
 }
 
 fixed_blocks = {
@@ -117,7 +133,87 @@ fixed_blocks = {
     (0,144,40,16),
     (24,144,16,88),
 #    (336,56,32,488),
-    ]
+    ],
+'g-06': [
+    (768,360,32,16),
+    (544,272,32,16),
+    (544,496,32,16),
+    (208,512,32,16),
+    (880,464,48,16),
+#   (456,512,128,32),
+    (456,512,128,8),
+    (128,312,32,232),
+    (0,632,40,72),
+    ],
+'g-07': [
+    (312,136,56,32),
+    (312,240,56,32),
+#    (240,152,32,192),
+#    (112,136i),
+    (336,432,32,104),
+    (336,616,32,64),
+    (240,648,32,16),
+    (336,824,32,32),
+    (0,1072,112,40),
+    (296,944,72,32),
+    (0,624,8,408),
+    ],
+'g-08': [
+    (1384,424,56,32),
+    (1208,256,32,32),
+    (1208,352,32,32),
+    (1320,296,32,32),
+    (1384,120,56,56),
+    (1080,152,32,32),
+    (1384,536,56,32),
+    (1112,448,32,16),
+    (1112,544,32,16),
+    (1112,616,32,32),
+    (840,400,32,32),
+    (736,360,32,16),
+    (704,560,32,16),
+    (488,280,16,16),
+    (416,264,16,16),
+    (896,88,32,16),
+    (600,128,32,16),
+    (504,88,32,16),
+    (400,144,32,16),
+    (416,184,16,16),
+    (480,208,16,16),
+    (400,216,16,16),
+    (464,240,16,16),
+    (0,144,64,32),
+    (0,0,8,128),
+    (0,104,24,24),
+    ],
+'g-10': [
+    (1008,288,64,32),
+    (880,232,16,72),
+    (816,288,32,16),
+    (0,184,48,16),
+    ],
+'g-11': [
+    (1208,520,48,16),
+    (0,400,72,16),
+    (656,600,16,16),
+    (664,344,16,16),
+    (904,752,32,16),
+    (1000,456,32,16),
+    (384,416,32,16),
+    ],
+'g-12': [
+    (680,560,56,16),
+    (368,784,80,16),
+    (288,464,16,16),
+    (272,576,32,16),
+    (312,168,16,16),
+    ],
+'g-13':[
+    (1280,104,72,16),
+    (1327,385,32,16),
+    (1007,121,32,16),
+    (1535,313,32,16),
+    ],
 }
 
 room_tiles = defaultdict(set)
@@ -128,11 +224,16 @@ room_plotters = defaultdict(render.Plotter)
 def map_tile(x,y):
     return math.floor(x/8), math.floor(y/8)
 
+durations = defaultdict(list)
+
 for run in runs:
     if tuw.ControlFlags.dead in run.control_flags:
         continue
 
     run_room = run.room_order[0]
+
+    durations[run_room].append(run.states[-1].timestamp-run.states[0].timestamp)
+
     for state in run.states:
         box = render.Plotter._state_box(state)
         center = [box[0] + box[2]/2, box[1]-box[3]/2]
@@ -175,4 +276,16 @@ for room_name, plotter in room_plotters.items():
         continue
     plotter.finalize()
     plotter.render(f'{room_name}.png', show=False)
+
+total = 0
+print('Durations:')
+for room_name, durs in durations.items():
+    if not 'g' in room_name: continue
+    if '14' in room_name: continue
+
+    avg = sum(durs)/len(durs)
+    avg = max(durs)
+    total += avg
+    print(f'{room_name}: {avg:.2f} ({durs})')
+print(f'\nTotal: {total:.2f} s')
 
