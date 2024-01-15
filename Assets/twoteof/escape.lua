aa8c7da3e7341544f91723a488a3bfa08192a67a
wlaub <goldcray@gmail.com>
2024-01-14 18:06:32 -0700

    Finalized bad end Added d-06 minitextbox Added f-05 secret Finalized hard ending

---
+++ Assets/twoteof/escape.lua
@@ -0,0 +1,10 @@
+
+function onBegin()
+    disableRetry()
+
+end
+
+function onLeave(player)
+    enableRetry()
+end
+
--- Assets/twoteof/mistake.lua
+++ Assets/twoteof/mistake.lua
@@ -13,8 +13,15 @@ function onBegin()
     player.Speed.Y = 0
     player.Speed.X = 0
 
+    local second_time = getFlag('Ch1_twoteof_bad_end')
+
     wait(2)
-    miniTextbox('waldmo_BadEnd')
+    if second_time then
+        miniTextbox('waldmo_BadEnd_2')
+    else
+        miniTextbox('waldmo_BadEnd')
+    end
+
     wait(5)
 
 
