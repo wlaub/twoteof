fe6a68514031835ded8e1d21b1bf86afd8212034
wlaub <goldcray@gmail.com>
2024-01-19 16:12:12 -0700

    checkpoint before jittering again

--- scripts/jitter.lua
+++ scripts/jitter.lua
@@ -13,13 +13,28 @@ local script = {
 
 function script.run(room, args)
     math.randomseed(os.time());
-    selection = selection_tool.getSelectionTargets() 
-    for _, entity in ipairs(selection) do
-        local offset_x = math.round(math.random()*2-1);
-        local offset_y = math.round(math.random()*2-1);
-        siu.moveSelection(room, layer, entity, offset_x, offset_y)
+--    selection = selection_tool.getSelectionTargets() 
+--    for _, entity in ipairs(selection) do
+--        if entity._name == "VivHelper/CustomSpinner" and entity.ShatterFlag == "" then
+--        if entity._name == "VivHelper/CustomSpinner" then
+--        entity.ShatterFlag = "test"
+--        if entity["ShatterFlag"] == "" then
+--            local offset_x = math.round(math.random()*2-1);
+--            local offset_y = math.round(math.random()*2-1);
+--            siu.moveSelection(room, layer, entity, offset_x, offset_y)
+--        end
+--    end
+--    selection_util.redrawTargetLayers(room, selection)
+
+    for _,entity in ipairs(room.entities) do
+        if entity._name == "VivHelper/CustomSpinner" and entity.ShatterFlag == "" then
+            local offset_x = math.round(math.random()*2-1);
+            local offset_y = math.round(math.random()*2-1);
+            entity.x = entity.x+offset_x
+            entity.y = entity.y+offset_y
+        end
+
     end
-    selection_util.redrawTargetLayers(room, selection)
 
 end
 
